e78f0269f3c7f746c4f860bd80bea597fd10ad36
vishwanath <vishwanathholge18@gmail.com>
2022-05-22 19:11:18 +0530

    Tic tac toe

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// const arr = [[0,0,0],[0,0,0],[0,0,0]];
+
+let grids = document.getElementsByClassName("container-item");
+
+
+const onClickEvent = (i) =>{
+    let mark = document.getElementById("grid");
+    console.log(grids)
+    grids[i].innerText = "X";
+    
+    
+    
+    
+
+
+
+
+}
+
