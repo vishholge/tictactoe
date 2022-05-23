5d47c38bf3cd751d7b54d6953972aaec0929d8f1
vishwanath <vishwanathholge18@gmail.com>
2022-05-23 19:47:28 +0530

    added some js part

--- js/index.js
+++ js/index.js
@@ -2,18 +2,42 @@
 
 let grids = document.getElementsByClassName("container-item");
 
+let flag = true;
+let player1 = document.getElementById("player1");
+let player2 = document.getElementById("player2");
 
+let turnplayer = document.getElementById("playing-player");
 const onClickEvent = (i) =>{
     let mark = document.getElementById("grid");
-    console.log(grids)
-    grids[i].innerText = "X";
+    if(flag === true){
+        grids[i].innerText = "X";
+        grids[i].style.backgroundColor = "red";
+        flag = false;
+        turnplayer.innerText = player2.value + "Your Turn";
+    }
+    else{
+        grids[i].innerText = "O";
+        grids[i].style.backgroundColor = "green";
+        flag = true;
+        turnplayer.innerText = player1.value + "Your Turn";
     
-    
-    
-    
-
-
-
+    }
+   
 
 }
+
+let gridTable = document.getElementById("grid-table");
+let  main =  document.getElementById("main");
+
+const playGame = () =>{
+
+    main.style.display = "none";
+    gridTable.style.display = "block";
+    turnplayer.innerText = player1.value + "  Your Turn"
+
+}
+
+const winningCombination = [
+    []
+]
 
